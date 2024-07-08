5312fd84f057bae1dea382d16769646890ef3ea4
Kacem ikbel <kassemikbel56@gmail.com>
2024-07-08 14:21:36 +0200

    about us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: AboutUsComponent },
   { path: 'footer', component: FooterComponent },
   { path: 'header', component: HeaderComponent },
   { path: 'portfolio', component: PortfolioComponent },
+  { path: 'aboutus', component: AboutUsComponent },
 ];
