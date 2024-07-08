a44ea6a172a4746fc9ef4ee937c5f32d460cf83d
Kacem ikbel <kassemikbel56@gmail.com>
2024-07-08 07:51:47 +0200

    footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 
 export const routes: Routes = [
 
      {
-      path:'', component : HomeComponent,
+      path:'', component : FooterComponent,
      },
 
    
